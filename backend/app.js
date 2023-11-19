0b996a1370dbd53df333f9efbbf1272cec778ccf
shermaine sng <shersmallsng@gmail.com>
2023-11-19 23:06:43 +0800

    added env

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,24 @@ const usersRoutes = require("./routes/users-routes");
 const eventsRoutes = require("./routes/events-routes");
 const pollsRoutes = require("./routes/polls-routes");
 const HttpError = require("./models/http-error");
-
 const app = express();
 app.use(bodyParser.json());
 // app.use(cors());
 
 // handling CORS error - exchange of data between different servers prohibited by browser
+// app.use(cors(
+//   {
+//     origin: ["https://deploy-mern-"]
+//   }
+// ))
+// if (process.env.ENV === "dev") require("dotenv").config();
+require("dotenv").config();
+
+/* Display message in the console if the connection is successful. */
+mongoose.connection.once("open", () => {
+  console.log("connected!");
+});
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -51,13 +63,25 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "an unknown error occurred" });
 });
 
+// mongoose
+//   .connect(
+//     "mongodb+srv://mainUser:newuser@cluster0.807aapy.mongodb.net/?retryWrites=true&w=majority"
+//   )
+//   .then(() => {
+//     app.listen(7005);
+//   })
+//   .catch((err) => {
+//     console.log("error is here " + err);
+//   });
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
+console.log("PORT:", process.env.PORT);
 mongoose
-  .connect(
-    "mongodb+srv://mainUser:newuser@cluster0.807aapy.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGODB_URI)
   .then(() => {
-    app.listen(7005);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
   })
   .catch((err) => {
-    console.log("error is here " + err);
+    console.log("Error connecting to MongoDB: " + err);
   });
